9a67c1c47d17990a8bea0293d24953f8758b06c2
aLpHa-Git-69 <alfazqureashi69@gmail.com>
2026-06-11 15:48:50 +0530

    camellia: optimizations and changes

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -6,3 +6,8 @@
 
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/lineage_camellia.mk
+
+COMMON_LUNCH_CHOICES := \
+    lineage_camellia-user \
+    lineage_camellia-userdebug \
+    lineage_camellia-eng
--- lineage_camellia.mk
+++ lineage_camellia.mk
@@ -22,6 +22,21 @@ PRODUCT_MODEL := M2103K19C
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
+# Dolby Audio
+# Set to false to disable Dolby Audio support at build time
+INCLUDE_DOLBY_AUDIO := true
+
+TARGET_ENABLE_BLUR := true
+TARGET_FACE_UNLOCK_SUPPORTED := true
+SYSTEM_OPTIMIZE_JAVA := true
+TARGET_BOOT_ANIMATION_RES := 1080
+SYSTEMUI_OPTIMIZE_JAVA := true
+TARGET_SUPPORTS_QUICK_TAP := true
+
+# Additional flags
+TORCH_STR_SUPPORTED := true
+TARGET_BUILD_DEVICE_AS_WEBCAM := true
+
 PRODUCT_BUILD_PROP_OVERRIDES += \
     BuildFingerprint=Redmi/camellia/camellia:12/SP1A.210812.016/V14.0.6.0.TKTMIXM:user/release-keys \
     DeviceProduct=$(PRODUCT_DEVICE)
